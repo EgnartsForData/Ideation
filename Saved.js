906bc54586bc7899f06c5dc4195af6d35eba8e13
EgnartsForData <61515082+EgnartsForData@users.noreply.github.com>
2020-02-26 18:55:38 +0100

    Create Saved.js

---
+++ Saved.js
@@ -0,0 +1,22 @@
+// Components/Saved.js
+
+import React from 'react'
+import { View, Text, Button, Image, StyleSheet} from 'react-native'
+
+class FirstView extends React.Component {
+  _goToConnexion() {
+    console.log("GO")
+  }
+
+  render() {
+
+    return (
+      <View>
+      <Text>Saved</Text>
+      </View>
+    )
+  }
+}
+
+
+export default FirstView
